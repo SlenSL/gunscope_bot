92fd019a2928bed41aa987507865e0e632e29b8b
SlenSL <55806194+SlenSL@users.noreply.github.com>
2022-09-17 16:32:14 +0300

    added first version of getting post updates from site

--- backend/controllers/BotController.php
+++ backend/controllers/BotController.php
@@ -60,10 +60,38 @@ class BotController extends Controller
 
     public function actionTest()
     {      
+        $postData = Yii::$app->request->post();
+
+        // echo '<pre>';
+        // var_dump($postData);
+        // echo '</pre>';
+        
+        $this->chatId = 712226559;
         $this->user = BotUser::getUser($this->chatId);
         $this->bot = new Bot('5780876936:AAGtj-8WeL-WlsE9QmzuH6URFTPxPd3EMI8', $this->chatId);
         
-        return $this->user->sendPostJson('https://andbots.ru/bot/get-posts');
+        // return json_decode($this->user->sendPostJson('https://andbots.ru/bot/get-posts'));
+        return (string) $this->user->sendPostJson('https://andbots.ru/bot/get-posts');
+
+        return http_response_code(200);
+    }
+
+    public function actionGetPosts()
+    {        
+        $postData = Yii::$app->request->post();
+        // $postData =  Json::decode(Yii::$app->request->post());
+
+        $this->chatId = 712226559;
+        $this->currentMessage = 'baza';
+        $this->bot = new Bot('5780876936:AAGtj-8WeL-WlsE9QmzuH6URFTPxPd3EMI8', $this->chatId);
+
+        // $this->sendMessage(
+        //     (string) $postData['username']
+        // );
+
+        return json_decode(file_get_contents("php://input"))[1]->postText; 
+        // return '<pre>'.print_r(json_decode(file_get_contents("php://input")),1).'</pre>';
+        // return 'a'; 
     }
 
 
